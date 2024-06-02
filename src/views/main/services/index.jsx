8a3063deed8e9f730e1fada1e0912faea8f15501
yusupheriharianto <yusupheriharianto@gmail.com>
2024-06-02 12:56:44 +0000

    refactor(main-services): remove unused variable on services page

--- src/views/main/services/index.jsx
+++ src/views/main/services/index.jsx
@@ -22,54 +22,19 @@
 */
 
 import React from "react";
-import { NavLink, useHistory } from "react-router-dom";
 // Chakra imports
 import {
   Box,
-  Button,
-  Checkbox,
   Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Input,
-  InputGroup,
-  InputRightElement,
-  FormErrorMessage,
-  chakra,
-  Stack,
-  HStack,
-  Text,
-  Link,
-  Icon,
   useColorModeValue,
-  SimpleGrid,
-  Skeleton,
-  Image,
 } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
 // Custom components
-import { HSeparator } from "components/separator/Separator";
 import DefaultMain from "layouts/main/Default";
-// Assets
-import illustration from "assets/img/auth/auth.jpg";
-import { FcGoogle } from "react-icons/fc";
-import { GoChevronRight } from "react-icons/go";
-import { MdBolt } from "react-icons/md";
-import { RiEyeCloseLine } from "react-icons/ri";
-
-import AuthService from "services/auth.services.js";
-
-import Heroes from "components/home/Heroes";
+// Assets;
 import Service from "components/home/Service";
-import Fleet from "components/home/Fleet";
-import Testimonial from "components/home/Testimonial";
 import Contact from "components/home/Contact";
-import Faq from "components/home/Faq";
 
 export default function Services() {
-  const { children, illustrationBackground } = illustration;
-
   function DottedBox() {
     return (
       <Box
@@ -109,33 +74,6 @@ export default function Services() {
         </svg>
       </Box>
     );
-  }
-  // Chakra color mode
-  const textColor = useColorModeValue("navy.700", "white");
-  const textColorSecondary = "gray.400";
-  const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
-  const textColorBrand = useColorModeValue("brand.500", "white");
-  const brandStars = useColorModeValue("brand.500", "brand.400");
-  const googleBg = useColorModeValue("secondaryGray.300", "whiteAlpha.200");
-  const googleText = useColorModeValue("navy.700", "white");
-  const googleHover = useColorModeValue(
-    { bg: "gray.200" },
-    { bg: "whiteAlpha.300" }
-  );
-  const googleActive = useColorModeValue(
-    { bg: "secondaryGray.300" },
-    { bg: "whiteAlpha.200" }
-  );
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
-  const history = useHistory();
-
-  function validateLogin(email) {
-    let error;
-    if (!email) {
-      error = "email or password is required";
-    }
-    return error;
   }
 
   return (
