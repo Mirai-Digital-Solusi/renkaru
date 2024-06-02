5c3bc5dc19eb3accc65340977297eb4f1b72f642
teofilushendry5 <teofilushendry5@gmail.com>
2024-06-02 13:08:55 +0000

    refactor(main-services): remove unused variables

--- src/views/main/services/index.jsx
+++ src/views/main/services/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 // Chakra imports
 import {
-  Box,
   Flex,
-  useColorModeValue,
 } from "@chakra-ui/react";
 // Custom components
 import DefaultMain from "layouts/main/Default";
@@ -12,46 +10,6 @@ import Service from "components/home/Service";
 import Contact from "components/home/Contact";
 
 export default function Services() {
-  function DottedBox() {
-    return (
-      <Box
-        position="absolute"
-        left="-45px"
-        top="-30px"
-        height="full"
-        maxW="700px"
-        zIndex={-1}
-      >
-        <svg
-          color={useColorModeValue(
-            "rgba(55,65,81, 0.1)",
-            "rgba(55,65,81, 0.7)"
-          )}
-          width="350"
-          height="420"
-          fill="none"
-        >
-          <defs>
-            <pattern
-              id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x="0" y="0" width="4" height="4" fill="currentColor"></rect>
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="404"
-            fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
-          ></rect>
-        </svg>
-      </Box>
-    );
-  }
 
   return (
     <DefaultMain>
