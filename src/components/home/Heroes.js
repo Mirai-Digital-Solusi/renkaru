9d84643732d136d9d3aab5319f1ea083d38d834b
teohrnt <teofilushendry5@gmail.com>
2024-06-03 20:13:23 +0700

    feat(main - home): update search modal to block heroes

--- src/components/home/Heroes.js
+++ src/components/home/Heroes.js
@@ -21,7 +21,7 @@
 
 */
 
-import React from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 // Chakra imports
 import {
@@ -47,6 +47,14 @@ import {
   Skeleton,
   Image,
   Container,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Select,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 // Custom components
@@ -60,10 +68,47 @@ import { MdBolt } from "react-icons/md";
 import Service from "components/home/Service";
 import { RiEyeCloseLine } from "react-icons/ri";
 
+import { createClient } from "@supabase/supabase-js";
 import AuthService from "services/auth.services.js";
+import UtilService from "services/util.service.js";
 
 export default function Heroes() {
   const { children, illustrationBackground } = illustration;
+
+  let DateFrom = UtilService.dateFrom;
+  let DateTo = UtilService.dateTo;
+
+  const supabase = createClient(
+    process.env.REACT_APP_API_KEY,
+    process.env.REACT_APP_ANON_KEY
+  );
+  const [data, setRentalOrders] = useState([]);
+  const [urlExcel, setUrlExcel] = useState("");
+
+
+
+  const {
+    isOpen: isOpenCreate,
+    onOpen: onOpenCreate,
+    onClose: onCloseCreate,
+  } = useDisclosure();
+
+  const [inputClientDateRentFrom, setClientDateRentFrom] = useState(DateFrom);
+  const [inputClientDateRentTo, setClientDateRentTo] = useState(DateTo);
+  const [inputClientCapacity, setClientCapacity] = useState(0);
+  const [inputClientRentType, setClientRentType] = useState();
+  const [inputImage, setImage] = useState(null);
+
+  const fleets = JSON.parse(localStorage.getItem("optionFleets"));
+
+  const rentType = [
+    {
+      option: "Private Rentals",
+    },
+    {
+      option: "Corporate Rentals",
+    },
+  ];
 
   function DottedBox() {
     return (
@@ -150,6 +195,162 @@ export default function Heroes() {
       justifyContent="start"
       direction="column"
     >
+      {/* Modal For Create Fleets */}
+      <Modal
+        isCentered
+        closeOnOverlayClick={false}
+        isOpen={isOpenCreate}
+        onClose={onCloseCreate}
+        size={"xl"}
+        scrollBehavior={"inside"}
+      >
+        <ModalOverlay
+          bg="blackAlpha.300"
+          backdropFilter="blur(10px) hue-rotate(0deg)"
+        />
+
+        <ModalContent>
+          <ModalHeader>Check Availability</ModalHeader>
+          <ModalBody>
+            {/* <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+              Client Name
+            </Text>
+            <Input
+              focusBorderColor="black"
+              placeholder="Input Client Name"
+              borderRadius="10px"
+              mb={3}
+              value={inputClientName}
+              onChange={(e) => setClientName(e.target.value)}
+            /> */}
+            {/* <HStack spacing="20px">
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Personal ID
+                </Text>
+                <Input
+                  focusBorderColor="black"
+                  placeholder="Input Personal ID"
+                  borderRadius="10px"
+                  mb={3}
+                  mr={5}
+                  value={inputClientPersonalID}
+                  onChange={(e) => setClientPersonalID(e.target.value)}
+                />
+              </Box>
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Rented Car
+                </Text>
+                <Select
+                  mr={20}
+                  w={"100%"}
+                  borderRadius="10px"
+                  mb={3}
+                  placeholder="Select option"
+                  value={inputClientRentedCar}
+                  onChange={(e) => setClientRentedCar(e.target.value)}
+                >
+                  {fleets.map((fleet, index) => (
+                    <option value={fleet.fleet_name}>{fleet.fleet_name}</option>
+                  ))}
+                </Select>
+              </Box>
+            </HStack> */}
+            <HStack spacing="24px">
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Date Rent From
+                </Text>
+                <Input
+                  w={"100%"}
+                  focusBorderColor="black"
+                  placeholder="Input Date Rent From"
+                  borderRadius="10px"
+                  mb={3}
+                  mr={10}
+                  type="datetime-local"
+                  value={inputClientDateRentFrom}
+                  onChange={(e) => {
+                    setClientDateRentFrom(e.target.value);
+                    getHours(e.target.value, inputClientDateRentTo);
+                  }}
+                />
+              </Box>
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Date Rent To
+                </Text>
+                <Input
+                  w={"100%"}
+                  focusBorderColor="black"
+                  placeholder="Input Date Rent To"
+                  borderRadius="10px"
+                  mb={3}
+                  mr={4}
+                  type="datetime-local"
+                  value={inputClientDateRentTo}
+                  onChange={(e) => {
+                    setClientDateRentTo(e.target.value);
+                    getHours(inputClientDateRentFrom, e.target.value);
+                  }}
+                />
+              </Box>
+            </HStack>
+            <HStack spacing="24px">
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Capacity
+                </Text>
+                <Input
+                  focusBorderColor="black"
+                  placeholder="Input Passenger Capacity"
+                  borderRadius="10px"
+                  mb={3}
+                  mr={5}
+                  value={inputClientCapacity}
+                  onChange={(e) => setClientCapacity(e.target.value)}
+                />
+              </Box>
+              <Box>
+                <Text fontSize="md" fontWeight={500} mb={1} ml={1}>
+                  Rent Type
+                </Text>
+                <Select
+                  mr={"4em"}
+                  w={"100%"}
+                  borderRadius="10px"
+                  mb={3}
+                  placeholder="Select Rent Type"
+                  value={inputClientRentType}
+                  onChange={(e) => setClientRentType(e.target.value)}
+                >
+                  {rentType.map((rentTypees, index) => (
+                    <option value={rentTypees.option}>{rentTypees.option}</option>
+                  ))}
+                </Select>
+              </Box>
+            </HStack>
+            <Image
+              url={inputImage}
+              previousImage={inputImage}
+              size={150}
+              onUpload={(event, url) => {
+                setImage(url);
+              }}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button mr={3} onClick={onCloseCreate}>
+              Cancel
+            </Button>
+            <Button colorScheme="facebook" onClick={() => insertFleets()}>
+              Search
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       <Flex
         maxW={{ base: "100%", md: "max-content" }}
         w="100%"
@@ -236,6 +437,7 @@ export default function Heroes() {
                   bgGradient: "linear(to-l, #0ea5e9,#2563eb)",
                   opacity: 0.9,
                 }}
+                onClick={() => {onOpenCreate();}}
               >
                 <chakra.span>Check Availability</chakra.span>
                 <Icon as={MdBolt} h={4} w={4} ml={1} />
