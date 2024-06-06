7fe2cabe8733acfce8ebcd960910681af1ba75a9
teohrnt <teofilushendry5@gmail.com>
2024-06-06 23:59:22 +0700

    feat(main - terms): update terms fetch api

--- src/components/home/Terms.js
+++ src/components/home/Terms.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Box,
   chakra,
@@ -14,6 +13,8 @@ import {
 } from "@chakra-ui/react";
 // Here we have used react-icons package for the icons
 import { FaRegNewspaper } from "react-icons/fa";
+import React, { useMemo, useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 const articles = [
   {
@@ -33,6 +34,26 @@ const articles = [
 ];
 
 export default function Terms() {
+
+    const supabase = createClient(
+        process.env.REACT_APP_API_KEY,
+        process.env.REACT_APP_ANON_KEY
+      );
+    
+      const [dataTerms, setTerms] = useState([]);
+    
+      useEffect(() => {
+        getTerms();
+    
+      }, []);
+    
+      async function getTerms() {
+        const { data } = await supabase.from("terms").select();
+        console.log("data services", data);
+        setTerms(data);
+      }
+
+      
   return (
     <Container maxWidth="4xl" p={{ base: 2, sm: 10 }}>
       <chakra.h3 fontSize="4xl" fontWeight="bold" textAlign="center">
@@ -42,46 +63,13 @@ export default function Terms() {
         Updated At 5 Dec 2023
       </chakra.h5>
       <Box position="relative"></Box>
-      <Box as="h2" fontSize="2xl" fontWeight="400" mt={5} textAlign="justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Box>
-      <Box as="h2" fontSize="2xl" fontWeight="400" mt={5} textAlign="justify">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
-      </Box>
-      <Box as="h2" fontSize="2xl" fontWeight="400" mt={5} textAlign="justify">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
-        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-        voluptatibus maiores alias consequatur aut perferendis doloribus
-        asperiores repellat.
-      </Box>
-      
+      {dataTerms.map((terms, index) => (
+      <><Box as="h2" fontSize="2xl" fontWeight="600" mt={5} textAlign="justify">
+              {terms.terms_title}
+          </Box><Box as="h2" fontSize="2xl" fontWeight="400" mt={5} textAlign="justify">
+                  {terms.terms_desc}
+              </Box></>
+       ))}
     </Container>
   );
 }
